76563a5a0b6189b95e8fbc848776c2e0fb54a8c6
shackerd <r.csgnl@pm.me>
2024-07-16 01:44:12 +0200

    finish connection status api

--- src/translators/connection.rs
+++ src/translators/connection.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use prometheus_exporter::prometheus::{core::GenericGauge, register_int_gauge};
+use prometheus_exporter::prometheus::{core::{AtomicI64, GenericGauge, MetricVec}, register_int_gauge, register_int_gauge_vec, GaugeVec, IntGauge, IntGaugeVec};
 use serde::Deserialize;
 
 use crate::core::common::{AuthenticatedHttpClientFactory, FreeboxResponse};
@@ -7,7 +7,7 @@ use crate::core::common::{AuthenticatedHttpClientFactory, FreeboxResponse};
 use super::TranslatorMetricTap;
 
 #[derive(Deserialize, Debug)]
-pub struct Connection {
+pub struct ConnectionStatus {
     #[serde(alias="type")]
     pub _type: String,
     pub rate_down: u64,
@@ -22,15 +22,24 @@ pub struct Connection {
     pub media: String
 }
 
+#[derive(Deserialize, Debug)]
+pub struct ConnectionConfiguration {
+
+}
+
 pub struct ConnectionTap {
     factory: AuthenticatedHttpClientFactory,
-    bytes_down_metric:  GenericGauge<prometheus_exporter::prometheus::core::AtomicI64>,
-    bytes_up_metric:  GenericGauge<prometheus_exporter::prometheus::core::AtomicI64>,
-    rate_down_metric:  GenericGauge<prometheus_exporter::prometheus::core::AtomicI64>,
-    rate_up_metric:  GenericGauge<prometheus_exporter::prometheus::core::AtomicI64>,
-    bandwidth_down_metric:  GenericGauge<prometheus_exporter::prometheus::core::AtomicI64>,
-    bandwidth_up_metric:  GenericGauge<prometheus_exporter::prometheus::core::AtomicI64>
-
+    bytes_down_metric:  IntGauge,
+    bytes_up_metric:  IntGauge,
+    rate_down_metric:  IntGauge,
+    rate_up_metric:  IntGauge,
+    bandwidth_down_metric:  IntGauge,
+    bandwidth_up_metric:  IntGauge,
+    type_metric: IntGaugeVec,
+    media_metric: IntGaugeVec,
+    state_metric: IntGaugeVec,
+    ipv4_metric: IntGaugeVec,
+    ipv6_metric: IntGaugeVec,
 }
 
 impl ConnectionTap {
@@ -44,7 +53,30 @@ impl ConnectionTap {
             rate_up_metric : register_int_gauge!("connection_rate_up", "connection_rate_up").expect("cannot create connection_rate_up gauge"),
             bandwidth_down_metric : register_int_gauge!("connection_bandwidth_down", "connection_bandwidth_down").expect("cannot create connection_bandwidth_down gauge"),
             bandwidth_up_metric : register_int_gauge!("connection_bandwidth_up", "connection_bandwidth_up").expect("cannot create connection_bandwidth_up gauge"),
+            type_metric : register_int_gauge_vec!("connection_type", "connection_type", &["type"]).expect("cannot create connection_type gauge"),
+            media_metric : register_int_gauge_vec!("connection_media", "connection_media", &["media"]).expect("cannot create connection_media gauge"),
+            state_metric : register_int_gauge_vec!("connection_state", "connection_state", &["state"]).expect("cannot create connection_state gauge"),
+            ipv4_metric : register_int_gauge_vec!("connection_ipv4", "connection_ipv4", &["ipv4"]).expect("cannot create connection_ipv4 gauge"),
+            ipv6_metric : register_int_gauge_vec!("connection_ipv6", "connection_ipv6", &["ipv6"]).expect("cannot create connection_ipv6 gauge")
         }
+    }
+
+    async fn get_connection_status(&self) -> Result<ConnectionStatus, Box<dyn std::error::Error>> {
+
+        let con_body =
+            self.factory.create_client().unwrap().get(format!("{}v4/connection", self.factory.api_url))
+            .send().await.unwrap()
+            .text().await.unwrap();
+
+        let con_res = serde_json::from_str::<FreeboxResponse<ConnectionStatus>>(&con_body);
+
+        let con = con_res.expect("Cannot read response").result;
+
+        Ok(con)
+    }
+
+    async fn get_connection_conf(&self) -> Result<ConnectionConfiguration, Box<dyn std::error::Error>> {
+        todo!()
     }
 }
 
@@ -52,21 +84,20 @@ impl ConnectionTap {
 impl TranslatorMetricTap for ConnectionTap {
 
     async fn set(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let body =
-            self.factory.create_client().unwrap().get(format!("{}v4/connection", self.factory.api_url))
-            .send().await.unwrap()
-            .text().await.unwrap();
 
-        let res = serde_json::from_str::<FreeboxResponse<Connection>>(&body);
+        let status = self.get_connection_status().await?;
 
-        let connection = res.expect("Cannot read response").result;
-
-        self.bytes_down_metric.set(connection.bytes_down as i64);
-        self.bytes_up_metric.set(connection.bytes_up as i64);
-        self.rate_down_metric.set(connection.rate_down as i64);
-        self.rate_up_metric.set(connection.rate_up as i64);
-        self.bandwidth_down_metric.set(connection.bandwidth_down as i64);
-        self.bandwidth_up_metric.set(connection.bandwidth_up as i64);
+        self.type_metric.with_label_values(&[status._type.as_str()]).set(1);
+        self.state_metric.with_label_values(&["up"]).set(if status.state == "up" { 1 } else { 0 } );
+        self.media_metric.with_label_values(&[status.media.as_str()]).set(1);
+        self.ipv4_metric.with_label_values(&[status.ipv4.as_str()]).set(1);
+        self.ipv6_metric.with_label_values(&[status.ipv6.as_str()]).set(1);
+        self.bytes_down_metric.set(status.bytes_down as i64);
+        self.bytes_up_metric.set(status.bytes_up as i64);
+        self.rate_down_metric.set(status.rate_down as i64);
+        self.rate_up_metric.set(status.rate_up as i64);
+        self.bandwidth_down_metric.set(status.bandwidth_down as i64);
+        self.bandwidth_up_metric.set(status.bandwidth_up as i64);
 
         Ok(())
     }
